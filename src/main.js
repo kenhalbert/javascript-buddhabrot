b9d81c9a44211ec5813dc1035693fc540d0dfafa
Ken Halbert <kennybhalbert@msn.com>
2017-09-06 15:08:37 +0000

    Set defaults to more reasonable values for showcase

--- src/main.js
+++ src/main.js
@@ -9,9 +9,9 @@ const drawer = createBuddhabrotDrawer(document.getElementById('canvas'), {
   imageScale: 600,
   plotScale: 600,  // TODO should this be calculated by the program every time?  pScale = Math.floor(pDim / 3) - easy
   plotDimensions: 1800,
-  sequenceEscapeThreshold: 15000,
+  sequenceEscapeThreshold: 250,
   sequenceBound: 2,
-  threads: 8,
+  threads: 1,
   color: {
     r: 255,
     g: 0, 
